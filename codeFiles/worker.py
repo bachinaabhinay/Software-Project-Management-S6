2d00e2f6ea70ffe4478c6f3adae823f299b56720
Tejareddy P <64905661+tejareddy41@users.noreply.github.com>
2022-03-23 00:03:10 +0530

    Update worker.py

--- codeFiles/worker.py
+++ codeFiles/worker.py
@@ -1,43 +1,51 @@
 import psycopg2
+import otherclass
 
 class worker:
-    def __init__(self,cur, myuid, mypass):
+    def __init__(self, cur, myuid, mypass):
         self.cur = cur
         self.myuid = myuid
         self.mypass = mypass
+        self.jcard = otherclass.jobcard(self.cur, self.myuid)
+        self.repdam = otherclass.damage_Report(self.cur,self.myuid)
 
     def updateMyPassword(self,newpass):
-
         if self.myuid == "" or self.mypass == "":
             print("Error!", "All fields are required")
         else:
             try:
-                self.cur.execute("select * from login where username=%s and password=%s",
+                self.cur.execute("select * from login where userid=%s and password=%s",
                             (self.myuid, self.mypass))
                 row = self.cur.fetchone()
                 if row == None:
-                    print("Error!", "Invalid USERNAME & PASSWORD")
+                    print("Error!", "Invalid userid & PASSWORD")
                 else:
                     try:
-                        self.cur.execute("update login set password=%s where username=%s and seckey=%s",
+                        self.cur.execute("update login set password=%s where userid=%s and seckey=%s",
                                     (newpass, self.myuid, self.mypass))
                         print("password changed")
                     except Exception as er:
-                        print("Error!", f"{er}")
+                        print("Erro", f"{er}")
             except Exception as er:
-                print("Error!", f"{er}")
+                print("Erro", f"{er}")
 
-    def addwork(self):
-        pass 
+    def view_mywork(self):
+        self.jcard.view_job_card(self.myuid)
+
+    def report_damage(self):
+        self.repdam.reportDamage()
+
 
 
 def main():
     connection = psycopg2.connect(host="localhost", user="postgres", password="root",
                                   database="inveMSys")
     cur = connection.cursor()
-    uname = input("Input UserNeme: ")
-    upass = input("Input Password: ")
-    addcs = worker(cur,uname,upass)
-    addcs.updateMyPassword(input("newpassword: "))
+    userid = input("input userid: ")
+    upass = input("input password: ")
+    addcs = worker(cur,userid,upass)
+    addcs.report_damage()
     connection.commit()
     connection.close()
+
+main()
