f196736284959bfeac96c179568c449f5b20d63d
Tejareddy P <64905661+tejareddy41@users.noreply.github.com>
2022-04-18 12:30:30 +0530

    Update login.py

--- src/login.py
+++ src/login.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk, Image
 import psycopg2
+import admin as adm
+import worker as wk
+import stockkeeper as stk
+import manager as mn
 
 
 class login:
@@ -15,9 +19,14 @@ class login:
         self.resize_image = file.resize((1250, 750))
         self.bg = ImageTk.PhotoImage(self.resize_image)
         self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0)
+        self.loginframe()
+
+    def loginframe(self):
         # login frame
         self.frame_login = Frame(self.root, bg="white")
         self.frame_login.place(x=675, y=100, height=320, width=500)
+        for widgets in self.frame_login.winfo_children():
+            widgets.destroy()
         title = Label(self.frame_login, text="Login", font=("Imapct", 28), bg="white").place(x=40, y=20)
 
         lb_user = Label(self.frame_login, text="Username", font=("Goudy old Style", 15), bg="white").place(x=40, y=90)
@@ -35,6 +44,8 @@ class login:
         Login_button.place(x=40, y=260)
 
     def validate_login(self):
+        self.a=self.txt_user.get()
+        self.p = self.txt_pass.get()
         if self.txt_user.get() == "" or self.txt_pass.get() == "":
             messagebox.showerror("Error!", "All fields are required", parent=self.root)
         else:
@@ -42,19 +53,38 @@ class login:
                 connection = psycopg2.connect(host="localhost", user="postgres", password="root",
                                               database="inveMSys")
                 cur = connection.cursor()
-                cur.execute("select * from login where userid=%s and password=%s",
-                            (self.txt_user.get(), self.txt_pass.get()))
+                cur.execute("select * from login where userid='{0}' and password='{1}'".format(self.a,self.p))
                 row = cur.fetchone()
                 if row == None:
                     messagebox.showerror("Error!", "Invalid USERNAME & PASSWORD", parent=self.root)
                 else:
-                    messagebox.showinfo("Success", "Login sucess", parent=self.root)
-                    # Clear all the entries
-                    self.reset_fields(2)
-                    connection.close()
-
+                    try:
+                        cur = connection.cursor()
+                        cur.execute("select role from employee where userid='{}'".format(self.a))
+                        row = cur.fetchone()
+                        self.acessframe(row[0])
+                        self.reset_fields(2)
+                    except Exception as e:
+                        messagebox.showerror("Error!", f"Error due to {str(e)}", parent=self.root)
+                connection.close()
             except Exception as e:
                 messagebox.showerror("Error!", f"Error due to {str(e)}", parent=self.root)
+
+    def acessframe(self,role):
+        if role == 'admin':
+            adm.main(self.a,self.p)
+        elif role == 'worker':
+            wk.main(self.a,self.p)
+        elif role == 'stockeeper':
+            stk.main(self.a,self.p)
+        elif role == 'manager':
+            mn.main(self.a,self.p)
+        else:
+            messagebox.showerror("No Page! Please Check", parent=self.root)
+            self.loginframe()
+
+
+
 
     def resetpass(self):
         for widgets in self.frame_login.winfo_children():
@@ -76,7 +106,9 @@ class login:
         clear = Button(self.frame_login, command=self.reset_fields, text="Clear", bg="white",font=("times new roman", 13))
         clear.place(x=45, y=270)
         save = Button(self.frame_login, command=self.savepassd, text="Save", bg="white",font=("times new roman", 13))
-        save.place(x=115, y=270)
+        save.place(x=110, y=270)
+        back = Button(self.frame_login, command=self.loginframe, text="Back", bg="white", font=("times new roman", 13))
+        back.place(x=175, y=270)
 
     def savepassd(self):
         uname = self.username.get()
